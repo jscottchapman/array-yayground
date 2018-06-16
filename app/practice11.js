a5588d9451344480d5a362178652ae4ae8855258
J Scott Chapman <scott8675309@gmail.com>
2018-06-16 11:47:18 -0600

    Added friendsters

---
+++ app/practice11.js
@@ -0,0 +1,101 @@
+let friendSters = [{
+    "id": 1,
+    "name": {
+      "first": "Jerry",
+      "last": "Jako"
+    },
+    "jobTitle": "Environmental Specialist",
+    "joined": 2016,
+    "friends": 80,
+    "avatar": "https://robohash.org/minusoptioaut.png?size=50x50&set=set1"
+  }, {
+    "id": 2,
+    "name": {
+      "first": "Brittne",
+      "last": "Bitchener"
+    },
+    "jobTitle": "Research Nurse",
+    "joined": 2010,
+    "friends": 15,
+    "avatar": "https://robohash.org/atestet.jpg?size=50x50&set=set1"
+  }, {
+    "id": 3,
+    "name": {
+      "first": "Fidole",
+      "last": "Urquhart"
+    },
+    "jobTitle": "Director of Sales",
+    "joined": 2016,
+    "friends": 93,
+    "avatar": "https://robohash.org/occaecatiquaeratdolorem.jpg?size=50x50&set=set1"
+  }, {
+    "id": 4,
+    "name": {
+      "first": "Carlita",
+      "last": "Clerk"
+    },
+    "jobTitle": "Office Assistant I",
+    "joined": 2017,
+    "friends": 31,
+    "avatar": "https://robohash.org/hiceaaut.jpg?size=50x50&set=set1"
+  }, {
+    "id": 5,
+    "name": {
+      "first": "Marjory",
+      "last": "Gaynes"
+    },
+    "jobTitle": "Editor",
+    "joined": 2010,
+    "friends": 22,
+    "avatar": "https://robohash.org/oditquosvoluptas.bmp?size=50x50&set=set1"
+  }, {
+    "id": 6,
+    "name": {
+      "first": "Annice",
+      "last": "Pease"
+    },
+    "jobTitle": "Accountant II",
+    "joined": 2013,
+    "friends": 32,
+    "avatar": "https://robohash.org/dignissimosdoloresearum.bmp?size=50x50&set=set1"
+  }, {
+    "id": 7,
+    "name": {
+      "first": "Monty",
+      "last": "Kleinber"
+    },
+    "jobTitle": "Assistant Media Planner",
+    "joined": 2015,
+    "friends": 42,
+    "avatar": "https://robohash.org/distinctiovoluptatemasperiores.png?size=50x50&set=set1"
+  }, {
+    "id": 8,
+    "name": {
+      "first": "Hubert",
+      "last": "Baptist"
+    },
+    "jobTitle": "Teacher",
+    "joined": 2013,
+    "friends": 100,
+    "avatar": "https://robohash.org/eteiusblanditiis.png?size=50x50&set=set1"
+  }, {
+    "id": 9,
+    "name": {
+      "first": "Felice",
+      "last": "Tortoishell"
+    },
+    "jobTitle": "Sales Representative",
+    "joined": 2012,
+    "friends": 82,
+    "avatar": "https://robohash.org/veroquoquasi.bmp?size=50x50&set=set1"
+  }, {
+    "id": 10,
+    "name": {
+      "first": "Shelton",
+      "last": "Sutheran"
+    },
+    "jobTitle": "Senior Cost Accountant",
+    "joined": 2011,
+    "friends": 5,
+    "avatar": "https://robohash.org/quianihilnesciunt.bmp?size=50x50&set=set1"
+  }]
