338ad983cb3306b69084fb50cacbbb6a250d3f02
J Scott Chapman <scott8675309@gmail.com>
2018-06-20 18:08:36 -0600

    added practice11.js

--- app/practice11.js
+++ app/practice11.js
@@ -1,101 +1,192 @@
-let friendSters = [{
+let friendFaces = [{
     "id": 1,
     "name": {
-      "first": "Jerry",
-      "last": "Jako"
+      "first": "Creight",
+      "last": "Tyrone"
     },
-    "jobTitle": "Environmental Specialist",
-    "joined": 2016,
-    "friends": 80,
-    "avatar": "https://robohash.org/minusoptioaut.png?size=50x50&set=set1"
+    "jobTitle": "Technical Writer",
+    "joined": 2011,
+    "friends": 59,
+    "avatar": "https://robohash.org/etinvoluptatem.bmp?size=200x200&set=set1",
+    "city": "El Paso",
+    "state": "Texas"
   }, {
     "id": 2,
     "name": {
-      "first": "Brittne",
-      "last": "Bitchener"
+      "first": "Allyson",
+      "last": "Ofener"
     },
-    "jobTitle": "Research Nurse",
-    "joined": 2010,
-    "friends": 15,
-    "avatar": "https://robohash.org/atestet.jpg?size=50x50&set=set1"
+    "jobTitle": "Community Outreach Specialist",
+    "joined": 2017,
+    "friends": 18,
+    "avatar": "https://robohash.org/enimnamrerum.jpg?size=200x200&set=set1",
+    "city": "Memphis",
+    "state": "Tennessee"
   }, {
     "id": 3,
     "name": {
-      "first": "Fidole",
-      "last": "Urquhart"
+      "first": "Alec",
+      "last": "Ferronier"
     },
-    "jobTitle": "Director of Sales",
-    "joined": 2016,
-    "friends": 93,
-    "avatar": "https://robohash.org/occaecatiquaeratdolorem.jpg?size=50x50&set=set1"
+    "jobTitle": "Assistant Manager",
+    "joined": 2010,
+    "friends": 7,
+    "avatar": "https://robohash.org/velutcorporis.jpg?size=200x200&set=set1",
+    "city": "Colorado Springs",
+    "state": "Colorado"
   }, {
     "id": 4,
     "name": {
-      "first": "Carlita",
-      "last": "Clerk"
+      "first": "Whitaker",
+      "last": "Humbatch"
     },
-    "jobTitle": "Office Assistant I",
-    "joined": 2017,
-    "friends": 31,
-    "avatar": "https://robohash.org/hiceaaut.jpg?size=50x50&set=set1"
+    "jobTitle": "Financial Analyst",
+    "joined": 2016,
+    "friends": 67,
+    "avatar": "https://robohash.org/doloremetducimus.jpg?size=200x200&set=set1",
+    "city": "Houston",
+    "state": "Texas"
   }, {
     "id": 5,
     "name": {
-      "first": "Marjory",
-      "last": "Gaynes"
+      "first": "Jillian",
+      "last": "Yankov"
     },
-    "jobTitle": "Editor",
-    "joined": 2010,
-    "friends": 22,
-    "avatar": "https://robohash.org/oditquosvoluptas.bmp?size=50x50&set=set1"
+    "jobTitle": "Operator",
+    "joined": 2012,
+    "friends": 89,
+    "avatar": "https://robohash.org/inquiavoluptatem.jpg?size=200x200&set=set1",
+    "city": "Canton",
+    "state": "Ohio"
   }, {
     "id": 6,
     "name": {
-      "first": "Annice",
-      "last": "Pease"
+      "first": "Gunner",
+      "last": "Bolsover"
     },
-    "jobTitle": "Accountant II",
-    "joined": 2013,
-    "friends": 32,
-    "avatar": "https://robohash.org/dignissimosdoloresearum.bmp?size=50x50&set=set1"
+    "jobTitle": "Sales Associate",
+    "joined": 2012,
+    "friends": 51,
+    "avatar": "https://robohash.org/autemetconsequatur.png?size=200x200&set=set1",
+    "city": "San Jose",
+    "state": "California"
   }, {
     "id": 7,
     "name": {
-      "first": "Monty",
-      "last": "Kleinber"
+      "first": "Ferd",
+      "last": "McKitterick"
     },
-    "jobTitle": "Assistant Media Planner",
-    "joined": 2015,
-    "friends": 42,
-    "avatar": "https://robohash.org/distinctiovoluptatemasperiores.png?size=50x50&set=set1"
+    "jobTitle": "Environmental Specialist",
+    "joined": 2014,
+    "friends": 96,
+    "avatar": "https://robohash.org/dictautpariatur.png?size=200x200&set=set1",
+    "city": "Atlanta",
+    "state": "Georgia"
   }, {
     "id": 8,
     "name": {
-      "first": "Hubert",
-      "last": "Baptist"
+      "first": "Neale",
+      "last": "Miguet"
     },
-    "jobTitle": "Teacher",
-    "joined": 2013,
-    "friends": 100,
-    "avatar": "https://robohash.org/eteiusblanditiis.png?size=50x50&set=set1"
+    "jobTitle": "VP Sales",
+    "joined": 2012,
+    "friends": 42,
+    "avatar": "https://robohash.org/providentminimaexplicabo.png?size=200x200&set=set1",
+    "city": "Providence",
+    "state": "Rhode Island"
   }, {
     "id": 9,
     "name": {
-      "first": "Felice",
-      "last": "Tortoishell"
+      "first": "Giralda",
+      "last": "Ivushkin"
     },
-    "jobTitle": "Sales Representative",
-    "joined": 2012,
-    "friends": 82,
-    "avatar": "https://robohash.org/veroquoquasi.bmp?size=50x50&set=set1"
+    "jobTitle": "Electrical Engineer",
+    "joined": 2014,
+    "friends": 25,
+    "avatar": "https://robohash.org/sedlaboreeaque.bmp?size=200x200&set=set1",
+    "city": "West Palm Beach",
+    "state": "Florida"
   }, {
     "id": 10,
     "name": {
-      "first": "Shelton",
-      "last": "Sutheran"
+      "first": "Pepi",
+      "last": "Gradly"
     },
-    "jobTitle": "Senior Cost Accountant",
-    "joined": 2011,
-    "friends": 5,
-    "avatar": "https://robohash.org/quianihilnesciunt.bmp?size=50x50&set=set1"
+    "jobTitle": "Database Administrator IV",
+    "joined": 2014,
+    "friends": 84,
+    "avatar": "https://robohash.org/etsuntdolores.png?size=200x200&set=set1",
+    "city": "Salinas",
+    "state": "California"
   }]
+
+
+// 1. write a function that prints out the full names of those
+// who have twenty or more friends and joined after 2015
+// 1st write a function that does the checking using  an
+// 'if' statement.Then use a 
+// for...of loop to call that function
+
+
+
+
+
+
+// // Answer:
+// let friendlyNoob = person => {
+//     if(person.friends > 19 && person.joined > 2015) {
+//         console.log(`${person.name.first} ${person.name.last}(${person.joined}) has ${person.friends} friends`);
+//     }
+// }
+// for(friendster of friendFaces) {
+//     friendlyNoob(friendster);
+// }
+
+// 2. We're looking for developers and engineers.  Print out
+// the first name and professions of those that include the
+// word 'developer' or 'engineer.'   If their profession 
+// doesn't have those words, print 'NEXT!' First create the
+// function that does the checking using if else and then pass it into
+// a forEach loop.
+
+
+//Answer 1: 
+//NOTES: show using ternary as well
+// function devsAndEngs(person) {
+//     let title = person.jobTitle.toLowerCase();
+//     if(title.includes('developer') || title.includes('engineer')) {
+//         console.log(`${person.name.first} is a ${person.jobTitle}`);
+//     } else {
+//         console.log('NEXT!');
+//     }
+//}
+
+//Answer 2 using ternary instead
+// function devsAndEngs(person) {
+//     let title = person.jobTitle.toLowerCase();
+//     (title.includes('developer') || title.includes('engineer')) ?
+//         console.log(`${person.name.first} is a ${person.jobTitle}`) :
+//         console.log('NEXT!');
+// }
+
+// friendsters.forEach(friendster => devsAndEngs(friendster));
+
+
+//TODO: demo how to do this with another problem
+
+// 3.  Refactor the previous function to take in a person 
+// and an array of keywords as strings as its two arguments and do the
+// same checking.  Then try it with ['Sales', and 'VP']
+
+// Answer
+// function devsAndEngs(person, array) {
+//     let title = person.jobTitle.toLowerCase();
+//     array.forEach(keyword => {
+//         title.includes(keyword.toLowerCase()) ?
+//         console.log(`${person.name.first}: ${person.jobTitle}`) :
+//         console.log('NEXT!');
+//     })
+// }
+
+// friendsters.forEach(friendster => devsAndEngs(friendster, ['Sales', 'VP']));
+
